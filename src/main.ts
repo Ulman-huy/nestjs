0476510b78deb7c445dc21b0bb4a6ba6b954e681
Ulman-huy <buihuy.lh@gmail.com>
2023-09-28 16:53:00 +0700

    save

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   // Add middleware
   app.useGlobalPipes(new ValidationPipe());
-  
-  await app.listen(4321, () => console.log('Server is re-started...!'));
+
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
